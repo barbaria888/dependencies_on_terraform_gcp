73391fe1d73643f9345fb4471c8f10c4a8f4b857
HARDIK ARORA <122469904+barbaria888@users.noreply.github.com>
2025-07-05 18:34:26 +0530

    Update external_dependencies(vm_bucket).tf

--- external_dependencies(vm_bucket).tf
+++ external_dependencies(vm_bucket).tf
@@ -1,24 +1,30 @@
-resource "google_compute_instance" "another_instance" {
 
-  name         = "terraform-instance-2"
-  machine_type = "e2-micro"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
+#another instance spins up
+resource "google_compute_instance" "another_instance"{
+    name="terraform-instance-2"
+    machine_type="e2-micro"
+    boot_disk{
+        initialize_params{
+            image="debian-cloud/debian-11"
+        }
     }
-  }
-  network_interface {
-    network = "default"
-    access_config {
+    network_interface{
+        network="default"
+        access_config{
+        }
     }
-  }
-  depends_on = [google_storage_bucket.example_bucket]
+#this instance dependent upon another storage bucket created
+depends_on=[google_storage_bucket.example_bucket]
+# the order of writing the code does not matter during creation, they are created on basis of implicit and explicit dependency always
+    }
+#a storage bucket spinning up
+resource "google_storage_bucket" "example_bucket"{
+    name= "qwiklabs-gcp-04-f7cce47df402_05_07"
+    location="US"
+    website{
+        main_page_suffix="index.html"
+
+        not_found_page="404.html"
+    }
 }
-resource "google_storage_bucket" "example_bucket" {
-  name     = "qwiklabs-gcp-03-a2eec700ffc4_04_07"
-  location = "US"
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-}
+
